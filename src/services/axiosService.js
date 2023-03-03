c3fa6ac7036bb872b2ff1ec2fc910b1d4df0fbfe
SEFSIS <sofiagryn@icloud.com>
2023-03-04 01:36:43 +0200

    lesson3(advance)

---
+++ src/services/axiosService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import {baseURL} from "../configs";
+
+
+
+
+const axiosService=axios.create({baseURL});
+
+export{
+    axiosService
+}
---
+++ src/validators/carValidator.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+const carValidator =Joi.object({
+    brand:Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
+        'string.pattern.base':'Тільки букви від 1 до 20 символів'
+    }),
+    price:Joi.number().min(0).max(1000000).required().messages({
+        'number.min':'Min 0'
+    }),
+    year:Joi.number().min(1990).max(new Date().getFullYear()).required()
+})
+
+export{
+    carValidator
+}
